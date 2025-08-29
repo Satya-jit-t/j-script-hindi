b9d3df67b2076c4806570fa23286c39a19e058ce
Satya-jit-t <situ.khatua2001@gmail.com>
2025-08-29 16:14:55 +0000

    container conf change

---
+++ basics_01/01_variable.js
@@ -0,0 +1,14 @@
+const accoundId=12345
+let accountMail="Satyajit@gmail.com"
+var accountCity="bhadrak"
+accoundState="Odisha" 
+let accountName
+
+
+
+accountMail="satyajiiiit"
+accountCity="Bhubaneswar"
+accountState="punjab"
+
+console.log(accoundId)
+console.table([accoundId,accountMail,accountCity,accountState,accountName])
