3ab7aca4d3ce9b74b76364f8c6109f2cf5bb4ff3
Satya-jit-t <situ.khatua2001@gmail.com>
2025-08-30 13:41:23 +0000

    conversion of operation

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score=33//number
+// let score="33abc"//string
+// let score=null//object
+//let score=undefined//undefined
+let score=true//for true it's 1 and 0 for false
+
+
+ let valueInNumber=Number(score)//convert string to number
+
+//console.log(typeof score);
+//console.log(typeof(score));
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);//actual value of a number is nan(not a number )
+
+
+//let isLoggedIn=1//for true its 1 and false for 0
+//let isLoggedIn=""//empty string is false
+let isLoggedIn="satyajit"
+let booleanIsLoggedIn=Boolean(isLoggedIn)//convert string to boolean
+//  console.log( booleanIsLoggedIn);
+//  console.log(typeof booleanIsLoggedIn);
+//  console.log(typeof isLoggedIn);
+let number=33
+let stringNumber=String(number)//number to string
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+ 
+ 
+ 
+ 
+
