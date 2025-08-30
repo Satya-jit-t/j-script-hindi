3b831b0bf2f7f20be7fd4dd823c218995d0e81b8
Satya-jit-t <situ.khatua2001@gmail.com>
2025-08-30 07:19:43 +0000

    cooment on reposetry

--- basics_01/01_variable.js
+++ basics_01/01_variable.js
@@ -3,7 +3,9 @@ let accountMail="Satyajit@gmail.com"
 var accountCity="bhadrak"
 accoundState="Odisha" 
 let accountName
-
+//we canot change the constant 
+//in java script we use let most of the cases let and var are changeable 
+//if we do not assign any value it bydefault take undefined
 
 
 accountMail="satyajiiiit"
