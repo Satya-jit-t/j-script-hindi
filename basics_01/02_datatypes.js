42213d1b05b85aaf35e2763db69aeac4177b07b3
Satya-jit-t <situ.khatua2001@gmail.com>
2025-08-30 08:08:11 +0000

    data types

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//use all js code as new version
+//alert(3+3) //we use node js not browser
+ console.log(3+3);
+ console.log("satyajit");
+  let name="sayajit"
+  let age=24
+  let isLoggedIn=true
+  let state;
+
+
+  //number=>2 to the power 53
+  //bigInt
+  //string=""
+  //boolean =>true/false
+  //null(its an object)=>stand alone value
+  //undefined
+  //symbol=>uniqe
+  console.log(typeof " satyajit");
+  console.log(typeof null);//object
+  console.log(typeof undefined);//undefined 
+  console.log(typeof age);
+  console.table([age,null,isLoggedIn,undefined])
+  
+  
+  
+  
+ 
+ 
