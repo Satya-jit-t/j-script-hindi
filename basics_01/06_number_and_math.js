79c1efb9a2b8fb7051fee6cb16a130cd6d53ae35
Satya-jit-t <situ.khatua2001@gmail.com>
2025-09-01 16:40:44 +0000

    data and time

---
+++ basics_01/06_number_and_math.js
@@ -0,0 +1,35 @@
+const number=100
+// console.log(number);
+const balance = new Number(100.121);
+// console.log(balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// console.log(balance.toPrecision(2));
+
+const hundred=100000000
+// console.log(hundred.toLocaleString('en-In'));//e-in for indian style commas
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.066));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.sqrt(101));
+// console.log(Math.min(3,4,5,6));
+
+// console.log(Math.max(3,4,5,6));
+
+console.log(Math.random());
+
+console.log(Math.random()+1);
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
---
+++ basics_01/07_date.js
@@ -0,0 +1,19 @@
+//node basics_01/07_date.js
+//Dates
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+
+
+// console.log(myDate);
+
+// let myCreateDate=new Date(2025,4,4)
+// let myCreateDate=new Date(2025,4,4,4,5)
+let myCreateDate=new Date("2025-02-03")
+console.log(myCreateDate.toLocaleString());
+
+console.log(myCreateDate.toDateString());
+
